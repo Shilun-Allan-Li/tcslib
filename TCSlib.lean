34b22a7a40333d5cf17647683f4f9a68d839097a
Shilun Li <47707803+Shilun-Allan-Li@users.noreply.github.com>
2026-01-22 16:51:13 -0800

    Update TCSlib.lean

--- TCSlib.lean
+++ TCSlib.lean
@@ -1,3 +1,6 @@
 import TCSlib.test
 import TCSlib.algorithms
 import TCSlib.CodingTheory.Basic
+import TCSlib.CodingTheory.CSS
+import TCSlib.CodingTheory.QuantumHamming
+import TCSlib.CodingTheory.QuantumSingleton
