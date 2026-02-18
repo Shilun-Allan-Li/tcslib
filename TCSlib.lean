2760513320d879172b820face817d0552b450669
Allan Li <allan_li3@apple.com>
2026-02-17 16:03:42 -0800

    remove css

--- TCSlib.lean
+++ TCSlib.lean
@@ -1,6 +1,5 @@
 -- import TCSlib.test
 -- import TCSlib.algorithms
 import TCSlib.CodingTheory.Basic
---import TCSlib.CodingTheory.CSS
 --import TCSlib.CodingTheory.QuantumHamming
 import TCSlib.CodingTheory.QuantumSingleton
